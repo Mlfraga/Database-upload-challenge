078e183ab72e9166db53fa19a08c96bf9e780ec5
Matheus Fraga <matheuslf44@gmail.com>
2020-09-16 11:35:54 -0300

    Fixing bugs :bug:

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,95 +1,51 @@
-import { getCustomRepository, getRepository, In } from 'typeorm';
-import csvParse from 'csv-parse';
-// import path from 'path';
 import fs from 'fs';
-import Transaction from '../models/Transaction';
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import Category from '../models/Category';
+import AppError from '../errors/AppError';
 
-// import uploadConfig from '../config/upload';
+import CreateTransactionService from './CreateTransactionService';
 
-interface LineTransactions {
+interface ResponseDTO {
   title: string;
+  type: 'income' | 'outcome';
   value: number;
-  type: 'income' | 'outcome';
-  category: string;
-}
-
-interface DataTransactions {
-  title: string;
-  value: string;
-  type: 'income' | 'outcome';
   category: string;
 }
 
 class ImportTransactionsService {
-  async execute(filePath: string): Promise<Transaction[]> {
-    const transactionRepository = getCustomRepository(TransactionsRepository);
-    const categoryRepository = getRepository(Category);
-
-    const contactsReadStream = fs.createReadStream(filePath);
-    // const fileCsvPath = path.join(uploadConfig.directory, filePath);
-
-    const parsers = csvParse({
-      from_line: 2,
-    });
-
-    const parsedCsv = contactsReadStream.pipe(parsers);
-
-    const transactions: LineTransactions[] = [];
-    const categories: string[] = [];
-
-    parsedCsv.on('data', (data: string[]) => {
-      const transaction: LineTransactions = {
-        title: data[0],
-        value: Number(data[2]),
-        type: data[1].trim() as 'income' | 'outcome',
-        category: data[3].trim(),
+  async execute(path: string): Promise<ResponseDTO[]> {
+    const fileExists = await fs.promises.stat(path);
+    if (!fileExists) {
+      throw new AppError('File does not exists!');
+    }
+    const Response: ResponseDTO[] = [];
+    const file = fs.readFileSync(path, 'utf-8');
+    const fileLine = file.split('\n');
+    const linesData = fileLine.splice(1, fileLine.length - 2);
+    linesData.map(line => {
+      const newData = line.split(', ');
+      const transaction: ResponseDTO = {
+        title: newData[0],
+        type: newData[1] === 'income' ? 'income' : 'outcome',
+        value: Number(newData[2]),
+        category: newData[3],
       };
-
-      transactions.push(transaction);
-      categories.push(transaction.category);
+      Response.push(transaction);
+      return transaction;
     });
 
-    await new Promise(resolve => parsedCsv.on('end', resolve));
+    const createTransaction = new CreateTransactionService();
+    console.log(linesData);
 
-    const categoriesFound = await categoryRepository.find({
-      where: {
-        title: In(categories),
-      },
-    });
+    for (const transaction of linesData) {
+      const newData = transaction.split(', ');
+      const createdTransaction = await createTransaction.execute({
+        title: newData[0],
+        type: newData[1] === 'income' ? 'income' : 'outcome',
+        value: Number(newData[2]),
+        category: newData[3],
+      });
+    }
 
-    const categoriesTitles = categoriesFound.map(row => row.title);
-
-    const selectedCategories = categories.filter(
-      category => !categoriesTitles.includes(category),
-    );
-    const selectedCategoriesFiltered = Array.from(new Set(selectedCategories));
-
-    const categoryTitle = selectedCategoriesFiltered.map(title => ({
-      title,
-    }));
-
-    const newCategories = categoryRepository.create(categoryTitle);
-
-    await categoryRepository.save(newCategories);
-    const allCategories = [...categoriesFound, ...newCategories];
-
-    const createdTransactions = transactionRepository.create(
-      transactions.map(transaction => ({
-        title: transaction.title,
-        type: transaction.type,
-        value: transaction.value,
-        category: allCategories.find(
-          category => category.title === transaction.title,
-        ),
-      })),
-    );
-
-    const savedTransactions = await transactionRepository.save(
-      createdTransactions,
-    );
-    return savedTransactions;
+    return Response;
   }
 }
 
